2eb29c60d4dece7542823e933c98225b4e026ef0
mohmedsabry <mayjak60@gmail.com>
2023-08-03 22:00:20 +0300

    update SplashActivity and Home

--- app/src/main/java/com/example/pharmashare/screens/Home.kt
+++ app/src/main/java/com/example/pharmashare/screens/Home.kt
@@ -1,7 +1,10 @@
 package com.example.pharmashare.screens
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.pharmashare.R
 
 class Home : AppCompatActivity() {
@@ -9,5 +12,23 @@ class Home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.main_profile -> {
+                Toast.makeText(baseContext, "${item.title}", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.main_setting -> {
+                Toast.makeText(baseContext, "${item.title}", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
     }
 }
