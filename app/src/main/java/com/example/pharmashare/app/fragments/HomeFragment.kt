6bbe0fd6e3e9db92ebe06641b269db5c2baaf880
mohmedsabry <mayjak60@gmail.com>
2023-08-16 21:24:58 +0300

    solved conflict problem

--- app/src/main/java/com/example/pharmashare/app/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/pharmashare/app/fragments/HomeFragment.kt
@@ -1,32 +1,31 @@
 package com.example.pharmashare.app.fragments
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.lifecycleScope
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
 import com.example.pharmashare.R
-import com.example.pharmashare.database.objects.Cart
-import com.example.pharmashare.database.objects.SharedMedicine
+import com.example.pharmashare.app.adptars.HomeAdapter
 import com.example.pharmashare.database.firebase.repos.SharedMedicineRepository
 import com.example.pharmashare.database.firebase.repos.UserRepository
+import com.example.pharmashare.database.objects.Cart
+import com.example.pharmashare.database.objects.SharedMedicine
 import com.example.pharmashare.database.room.MyRoomDatabase
 import com.example.pharmashare.database.room.repo.CartRepo
-import com.example.pharmashare.app.adptars.HomeAdapter
-import com.example.pharmashare.database.firebase.repos.PharmacyRepository
 
 class HomeFragment : Fragment(), HomeAdapter.HomeListener {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var homeAdapter: HomeAdapter
+
     companion object {
         private lateinit var database: MyRoomDatabase
     }
+
     private lateinit var dao: CartRepo
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,24 +40,43 @@ class HomeFragment : Fragment(), HomeAdapter.HomeListener {
         database = MyRoomDatabase.buildDatabase(requireContext())
         dao = database.cartDao()
         val userId = UserRepository.getCurrentUserId()
-        SharedMedicineRepository.getAllSharedMedicines(userId) { sharedMedicines ->
+        SharedMedicineRepository.getAllSharedMedicinesWithQuantity(userId) { sharedMedicines ->
             homeAdapter = HomeAdapter(sharedMedicines, this)
             recyclerView.adapter = homeAdapter
         }
 
         return rootView
     }
-    override fun addToCart(sharedMedicine: SharedMedicine, selectedQuantity: Int, price: Double,pharmacyId: String) {
+
+    override fun addToCart(
+        sharedMedicine: SharedMedicine,
+        selectedQuantity: Int,
+        price: Double,
+        pharmacyId: String
+    ): Boolean {
         val cart = Cart(
             pharmacyId = pharmacyId,
             medicine = sharedMedicine.medicineName,
             quantity = selectedQuantity,
             price = price,
             priceTotal = selectedQuantity * price,
-            availableQuantity = sharedMedicine.quantity
+            availableQuantity = sharedMedicine.quantity,
+            sharedMedicineId = sharedMedicine.id
         )
-        synchronized(cart){
+
+        synchronized(cart) {
             dao.insertByRoom(cart)
+        }
+
+        // check if medicine exist in cart if yes show message else add it
+        val cartItem = dao.checkIfMedicineExist(sharedMedicineId = sharedMedicine.id)
+        return if (cartItem == null) {
+            dao.insertByRoom(cart)
+            true
+        } else {
+            Toast.makeText(context, "This medicine already exist in cart", Toast.LENGTH_SHORT)
+                .show()
+            false
         }
     }
 }
